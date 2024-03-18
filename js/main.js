e8a80c8199bab3451f8775f18c2e1467180e126e
Kynan Van Looy <r0983516@student.thomasmore.be>
2024-03-18 16:38:55 +0100

    adding javascript

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var slider = document.getElementById("SliderWater");
+
+function adjustSlider(){
+    valueSlider = slider.value
+
+
+    switch (valueSlider){
+        
+    }
+}
+
+adjustSlider();
+
+slider.addEventListener('input', adjustSlider);
