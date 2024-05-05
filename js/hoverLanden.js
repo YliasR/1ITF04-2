fd06c01a13744b24cf9b1d78f1d8679bc5cd748a
sanderwijns <r0837395@student.thomasmore.be>
2024-05-05 16:57:14 +0200

    popups

--- js/hoverLanden.js
+++ js/hoverLanden.js
@@ -1,12 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
   var svgElements = document.querySelectorAll(".svg-overlay circle");
-  var popup = document.getElementById("popup");
+  var popups = document.querySelectorAll(".popup");
+  var popupBelgië = document.getElementById("popupBelgië");
+  var popupNederland = document.getElementById('popupNederland');
+  var popupFrankrijk = document.getElementById('popupFrankrijk');
   var belgië = document.getElementById("België");
   var belgiëText = document.getElementById("BelgiëText");
   var frankrijk = document.getElementById("Frankrijk");
   var frankrijkText = document.getElementById("FrankrijkText");
   var nederland = document.getElementById("Nederland");
   var nederlandText = document.getElementById("NederlandText");
+  var slider = document.getElementById('SliderWaterLevel');
 
   belgië.addEventListener("mouseover", function(event) {
     belgiëText.style.visibility = "visible"; // Show text on hover
@@ -36,22 +40,131 @@ document.addEventListener("DOMContentLoaded", function() {
     element.addEventListener("click", function(event) {
       var x = event.clientX;
       var y = event.clientY;
-      popup.style.left = x-800 + "px";
-      popup.style.top = y-200 + "px";
-      popup.textContent = "Info over " + event.target.id + ":";
-      popup.style.display = "block";
+      var land = event.target.id;
+      if (land == "België"){
+        popupBelgië.setAttribute('style', 'white-space: pre;');
+        var sliderWaarde = slider.value;
+        switch (sliderWaarde){
+          case '0':
+            popupBelgië.textContent = "België 0m \r\n";
+            popupBelgië.textContent += "Oppervlakte onder water: \r\n";
+            popupBelgië.textContent += "Schade per km²: "
+            break;
+          case '1':
+            popupBelgië.textContent = "België 5m \r\n";
+            popupBelgië.textContent += "Oppervlakte onder water: \r\n";
+            popupBelgië.textContent += "Schade per km²: "
+            break;
+          case '2':
+            popupBelgië.textContent = "België 15 \r\n";
+            popupBelgië.textContent += "Oppervlakte onder water: \r\n";
+            popupBelgië.textContent += "Schade per km²: "
+            break;
+          case '3':
+            popupBelgië.textContent = "België 25m \r\n";
+            popupBelgië.textContent += "Oppervlakte onder water: \r\n";
+            popupBelgië.textContent += "Schade per km²: "
+            break;
+          case '4':
+            popupBelgië.textContent = "België 50m \r\n";
+            popupBelgië.textContent += "Oppervlakte onder water: \r\n";
+            popupBelgië.textContent += "Schade per km²: "
+            break;
+          case '5':
+            popupBelgië.textContent = "België 100m \r\n";
+            popupBelgië.textContent += "Oppervlakte onder water: \r\n";
+            popupBelgië.textContent += "Schade per km²: "
+            break;
+        }
+        popupBelgië.style.left = x-800 + "px";
+        popupBelgië.style.top = y-200 + "px";
+        popupBelgië.style.display = "block";
+      } else if (land == 'Nederland'){
+        popupNederland.setAttribute('style', 'white-space: pre;');
+        var sliderWaarde = slider.value;
+        switch (sliderWaarde){
+          case '0':
+            popupNederland.textContent = "Nederland 0m \r\n";
+            popupNederland.textContent += "Oppervlakte onder water: \r\n";
+            popupNederland.textContent += "Schade per km²: "
+            break;
+          case '1':
+            popupNederland.textContent = "Nederland 5m \r\n";
+            popupNederland.textContent += "Oppervlakte onder water: \r\n";
+            popupNederland.textContent += "Schade per km²: "
+            break;
+          case '2':
+            popupNederland.textContent = "Nederland 15m \r\n";
+            popupNederland.textContent += "Oppervlakte onder water: \r\n";
+            popupNederland.textContent += "Schade per km²: "
+            break;
+          case '3':
+            popupNederland.textContent = "Nederland 25m \r\n";
+            popupNederland.textContent += "Oppervlakte onder water: \r\n";
+            popupNederland.textContent += "Schade per km²: "
+            break;
+          case '4':
+            popupNederland.textContent = "Nederland 50m \r\n";
+            popupNederland.textContent += "Oppervlakte onder water: \r\n";
+            popupNederland.textContent += "Schade per km²: "
+            break;
+          case '5':
+            popupNederland.textContent = "Nederland 100m \r\n";
+            popupNederland.textContent += "Oppervlakte onder water: \r\n";
+            popupNederland.textContent += "Schade per km²: "
+            break;
+        }
+        popupNederland.style.left = x-800 + "px";
+        popupNederland.style.top = y-200 + "px";
+        popupNederland.style.display = "block";
+      } else if (land == 'Frankrijk') {
+        popupFrankrijk.setAttribute('style', 'white-space: pre;');
+        var sliderWaarde = slider.value;
+        switch (sliderWaarde){
+          case '0':
+            popupFrankrijk.textContent = "Frankrijk 0m \r\n";
+            popupFrankrijk.textContent += "Oppervlakte onder water: \r\n";
+            popupFrankrijk.textContent += "Schade per km²: "
+            break;
+          case '1':
+            popupFrankrijk.textContent = "Frankrijk 5m \r\n";
+            popupFrankrijk.textContent += "Oppervlakte onder water: \r\n";
+            popupFrankrijk.textContent += "Schade per km²: "
+            break;
+          case '2':
+            popupFrankrijk.textContent = "Frankrijk 15m \r\n";
+            popupFrankrijk.textContent += "Oppervlakte onder water: \r\n";
+            popupFrankrijk.textContent += "Schade per km²: "
+            break;
+          case '3':
+            popupFrankrijk.textContent = "Frankrijk 25m \r\n";
+            popupFrankrijk.textContent += "Oppervlakte onder water: \r\n";
+            popupFrankrijk.textContent += "Schade per km²: "
+            break;
+          case '4':
+            popupFrankrijk.textContent = "Frankrijk 50m \r\n";
+            popupFrankrijk.textContent += "Oppervlakte onder water: \r\n";
+            popupFrankrijk.textContent += "Schade per km²: "
+            break;
+          case '5':
+            popupFrankrijk.textContent = "Frankrijk 100m \r\n";
+            popupFrankrijk.textContent += "Oppervlakte onder water: \r\n";
+            popupFrankrijk.textContent += "Schade per km²: "
+            break;
+        }
+        popupFrankrijk.style.left = x-800 + "px";
+        popupFrankrijk.style.top = y-200 + "px";
+        popupFrankrijk.style.display = "block";
+      }
     });
   });
 
-
   document.addEventListener("click", function(event) {
-    if (!event.target.closest(".svg-overlay")) {
-      popup.style.display = "none";
+    if (!event.target.closest(".svg-overlay circle")) {
+      popupBelgië.style.display = "none";
+      popupNederland.style.display = "none";
+      popupFrankrijk.style.display = "none";
     }
-  });
-
-  document.getElementById("WaterFloodMap").addEventListener("click", function(event) {
-    popup.style.display = "none";
   });
 
 });
